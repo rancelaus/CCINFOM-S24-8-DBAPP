0ff83917863d5ccfb42499df2ace5b889ba56f90
rancelaus <rance_laus@dlsu.edu.ph>
2025-11-19 15:03:50 +0800

    some few fixes for inventory

--- AssetManagementApp/src/java/inventorymanagement/Inventory.java
+++ AssetManagementApp/src/java/inventorymanagement/Inventory.java
@@ -4,6 +4,7 @@
  */
 package inventorymanagement;
 import java.util.*;
+import java.sql.*;
 
 /**
  *
@@ -22,10 +23,35 @@ public class Inventory {
     // Array Lists
     public ArrayList<Integer> itemIDlist = new ArrayList<>();
     public ArrayList<String> itemNamelist = new ArrayList<>();
+    public ArrayList<String> itemTypelist = new ArrayList<>();
+    public ArrayList<String> unitlist = new ArrayList<>();
+    public ArrayList<Integer> stockQtylist = new ArrayList<>();
+    public ArrayList<Integer> reoderLevellist = new ArrayList<>();
+    public ArrayList<String> i_statuslist = new ArrayList<>();
     
     public Inventory() {}
     
     public int createInventory() {
+        try {
+            // Connecting to Database
+            Connection conn;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/asset_management_db?useTimezone=true&serverTimezone=UTC&user=root&password=12345678");
+            System.out.println("Connection Succesful");
+            
+            // Prepare SQL Statement
+            PreparedStatement pstmt = conn.prepareStatement("SELECT MAX(itemID) + 1 AS newID FROM inventory");
+            ResultSet rst = pstmt.executeQuery();
+            while (rst.next()) {
+                itemID = rst.getInt("newID");
+            }
+            
+            // Save the new Inventory
+            pstmt = conn.prepareStatement("INSERT INTO inventory (itemID, itemName, itemType, unit, stockQty, reorderLevel, i_status) VALUE (?, ?, ?, ?, ?, ?, ?)");
+            pstmt.setInt(1, itemID);
+            pstmt.setString(2, itemName);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return 0;
     }
     
